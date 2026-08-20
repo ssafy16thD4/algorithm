77c265ebb3c8a64fdccec938a3ebbea66b6e1db5
LeeSeongIl <lsimma7@naver.com>
2026-08-20 18:00:03 +0900

    add:여행경로.java

--- 이성일/week4/Testcode.java
+++ 이성일/week4/Testcode.java
@@ -1,24 +1,29 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Testcode {
     public static void main(String[] args) {
-
-        Set<Integer> s = new HashSet<>();
-        Set<Set<Integer>> ss = new HashSet<>();
-        ss.add(s);
-        s.add(1);
-        s.add(2);
-        s.add(3);
-        Set<Integer> s1 = new HashSet<>();
-        s1.add(1);
-        s1.add(2);
-        s1.add(3);
-        for (Set<Integer> ex : ss) {
-            if (s1.equals(ex))
-                System.out.println("true");
+        String[][] tickets = { { "ICN", "SFO" }, { "ICN", "ATL" }, { "SFO", "ATL" }, { "ATL", "ICN" },
+                { "ATL", "SFO" } };
+        Map<String, List<String>> routes = new HashMap<>();
+        List<String> sources;
+        Set<String> set = new HashSet<>();
+        for (String[] ticket : tickets) {
+            if (routes.containsKey(ticket[0])) {
+                routes.get(ticket[0]).add(ticket[1]);
+            } else {
+                routes.put(ticket[0], new ArrayList<>());
+                routes.get(ticket[0]).add(ticket[1]);
+            }
+            set.add(ticket[0]);
+            set.add(ticket[1]);
+        }
+        sources = new ArrayList<>(set);
+        sources.sort((o1, o2) -> o1.compareTo(o2));
+        for (String source : sources) {
+            System.out.println(source);
+        }
+        for (String source : sources) {
+            routes.get(source).sort((o1, o2) -> o1.compareTo(o2));
         }
     }
 }
