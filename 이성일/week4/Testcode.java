e2b3599542d30202a30863d50effa678f7a4d60d
LeeSeongIl <lsimma7@naver.com>
2026-08-21 16:05:25 +0900

    add:징검다리건너기.java,여행경로.java

--- 이성일/week4/Testcode.java
+++ 이성일/week4/Testcode.java
@@ -2,28 +2,18 @@ import java.util.*;
 
 public class Testcode {
     public static void main(String[] args) {
-        String[][] tickets = { { "ICN", "SFO" }, { "ICN", "ATL" }, { "SFO", "ATL" }, { "ATL", "ICN" },
-                { "ATL", "SFO" } };
-        Map<String, List<String>> routes = new HashMap<>();
-        List<String> sources;
-        Set<String> set = new HashSet<>();
-        for (String[] ticket : tickets) {
-            if (routes.containsKey(ticket[0])) {
-                routes.get(ticket[0]).add(ticket[1]);
-            } else {
-                routes.put(ticket[0], new ArrayList<>());
-                routes.get(ticket[0]).add(ticket[1]);
+        int[] stones = new int[] { 2, 4, 5, 3, 2, 1, 4, 2, 5, 1 };
+        int minimum = 200000001, maximum = 0;
+        for (int i = 0; i < stones.length; i++) {
+            if (stones[i] < minimum) {
+                minimum = stones[i];
             }
-            set.add(ticket[0]);
-            set.add(ticket[1]);
+            if (stones[i] > maximum)
+                maximum = stones[i];
         }
-        sources = new ArrayList<>(set);
-        sources.sort((o1, o2) -> o1.compareTo(o2));
-        for (String source : sources) {
-            System.out.println(source);
-        }
-        for (String source : sources) {
-            routes.get(source).sort((o1, o2) -> o1.compareTo(o2));
-        }
+        System.out.println(minimum + "" + maximum);
+
+        Deque<Integer> deq = new ArrayDeque<>();
+        deq.peekFirst()
     }
 }
